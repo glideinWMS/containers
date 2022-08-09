3558f8b2a70a41049decaad5e917759fbd176ae9
AidanHimley <ah8uqq@virginia.edu>
2022-08-08 20:02:15 -0500

    add helm chart for frontend and condor-cm

---
+++ frontend/helm/clean-creds.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# util script to clear automatically generated passwords and tokens for debugging
+rm persistent-mount/condor/passwords.d/POOL persistent-mount/condor/passwords.d/FRONTEND \
+    persistent-mount/condor/tokens.d/condor.idtoken \
+    persistent-mount/frontend/passwords.d/FRONTEND \
+    persistent-mount/frontend/tokens.d/frontend.$HOSTNAME.idtoken \
+    persistent-mount/output-tokens/*
