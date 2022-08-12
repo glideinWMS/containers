b10b7f49b0861a283da5c1b3356b33aa79535a52
AidanHimley <ah8uqq@virginia.edu>
2022-08-12 16:13:06 -0500

    clean up templates and point to a dockerhub image

--- frontend/helm/config/frontend/setup-creds.sh
+++ frontend/helm/config/frontend/setup-creds.sh
@@ -1,15 +1,15 @@
-if [[ ! -s /etc/condor/passwords.d/FRONTEND ]]; then
+if [[ ! -s /var/lib/gwms-frontend/passwords.d/FRONTEND ]]; then
     ( flock -n 200 || exit 0;
     echo "FRONTEND is empty";
-    condor_store_cred add -f /etc/condor/passwords.d/FRONTEND -p `/etc/condor/passwords.d/passgen.py`;
-    chmod 600 /etc/condor/passwords.d/FRONTEND;
-    cp /etc/condor/passwords.d/FRONTEND /root/condor-passwords.d;
-    chown -R frontend.frontend /etc/condor/tokens.d/ /etc/condor/passwords.d/;
-    ) 200>/etc/condor/passwords.d/FRONTEND;
+    condor_store_cred add -f /var/lib/gwms-frontend/passwords.d/FRONTEND -p `/var/lib/gwms-frontend/passwords.d/passgen.py`;
+    chmod 600 /var/lib/gwms-frontend/passwords.d/FRONTEND;
+    cp /var/lib/gwms-frontend/passwords.d/FRONTEND /root/condor-passwords.d;
+    chown -R frontend.frontend /var/lib/gwms-frontend/.condor/tokens.d/ /var/lib/gwms-frontend/passwords.d/;
+    ) 200>/var/lib/gwms-frontend/passwords.d/FRONTEND;
 else
     echo "FRONTEND is not empty";
 fi;
 
-until [[ -s /etc/condor/passwords.d/FRONTEND ]]; do
+until [[ -s /var/lib/gwms-frontend/passwords.d/FRONTEND ]]; do
     sleep 1;
 done;
